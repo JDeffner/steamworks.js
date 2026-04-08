1f2d49af05a6b4d01a46677c1d6687064a09a469
ceifa <gabrinelson27@gmail.com>
2026-04-07 21:06:58 -0300

    refact: cleanup callback handlers with a macro

--- src/api/callback.rs
+++ src/api/callback.rs
@@ -4,6 +4,7 @@ use napi_derive::napi;
 pub mod callback {
     use crate::api::FatalTsfn;
     use napi::threadsafe_function::ThreadsafeFunctionCallMode;
+    use napi_derive::napi;
 
     #[napi]
     pub struct Handle {
@@ -20,8 +21,36 @@ pub mod callback {
         }
     }
 
-    #[napi]
-    pub enum SteamCallback {
+    macro_rules! steam_callbacks {
+        ($($variant:ident),* $(,)?) => {
+            #[napi]
+            pub enum SteamCallback {
+                $($variant),*
+            }
+
+            #[napi(ts_generic_types = "C extends keyof import('./callbacks').CallbackReturns")]
+            pub fn register(
+                #[napi(ts_arg_type = "C")] steam_callback: SteamCallback,
+                #[napi(ts_arg_type = "(value: import('./callbacks').CallbackReturns[C]) => void")] handler: FatalTsfn<serde_json::Value>,
+            ) -> Handle {
+                let threadsafe_handler = handler;
+
+                let handle = match steam_callback {
+                    $(
+                        SteamCallback::$variant => {
+                            register_callback::<steamworks::$variant>(threadsafe_handler)
+                        }
+                    )*
+                };
+
+                Handle {
+                    handle: Some(handle),
+                }
+            }
+        };
+    }
+
+    steam_callbacks!(
         PersonaStateChange,
         SteamServersConnected,
         SteamServersDisconnected,
@@ -32,52 +61,7 @@ pub mod callback {
         P2PSessionConnectFail,
         GameLobbyJoinRequested,
         MicroTxnAuthorizationResponse,
-    }
-
-    #[napi(ts_generic_types = "C extends keyof import('./callbacks').CallbackReturns")]
-    pub fn register(
-        #[napi(ts_arg_type = "C")] steam_callback: SteamCallback,
-        #[napi(ts_arg_type = "(value: import('./callbacks').CallbackReturns[C]) => void")] handler: FatalTsfn<serde_json::Value>,
-    ) -> Handle {
-        let threadsafe_handler = handler;
-
-        let handle = match steam_callback {
-            SteamCallback::PersonaStateChange => {
-                register_callback::<steamworks::PersonaStateChange>(threadsafe_handler)
-            }
-            SteamCallback::SteamServersConnected => {
-                register_callback::<steamworks::SteamServersConnected>(threadsafe_handler)
-            }
-            SteamCallback::SteamServersDisconnected => {
-                register_callback::<steamworks::SteamServersDisconnected>(threadsafe_handler)
-            }
-            SteamCallback::SteamServerConnectFailure => {
-                register_callback::<steamworks::SteamServerConnectFailure>(threadsafe_handler)
-            }
-            SteamCallback::LobbyDataUpdate => {
-                register_callback::<steamworks::LobbyDataUpdate>(threadsafe_handler)
-            }
-            SteamCallback::LobbyChatUpdate => {
-                register_callback::<steamworks::LobbyChatUpdate>(threadsafe_handler)
-            }
-            SteamCallback::P2PSessionRequest => {
-                register_callback::<steamworks::P2PSessionRequest>(threadsafe_handler)
-            }
-            SteamCallback::P2PSessionConnectFail => {
-                register_callback::<steamworks::P2PSessionConnectFail>(threadsafe_handler)
-            }
-            SteamCallback::GameLobbyJoinRequested => {
-                register_callback::<steamworks::GameLobbyJoinRequested>(threadsafe_handler)
-            }
-            SteamCallback::MicroTxnAuthorizationResponse => {
-                register_callback::<steamworks::MicroTxnAuthorizationResponse>(threadsafe_handler)
-            }
-        };
-
-        Handle {
-            handle: Some(handle),
-        }
-    }
+    );
 
     fn register_callback<C>(
         threadsafe_handler: FatalTsfn<serde_json::Value>,
